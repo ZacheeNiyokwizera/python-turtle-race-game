b764b13d0e0ecae335e0ded0477822c25e3642be
Zachee <zack@labs.ws>
2023-06-08 18:07:35 +0100

    this is fun Hahaha (:

---
+++ test.py
@@ -0,0 +1,23 @@
+# from turtle import Turtle, Screen
+# # from turtle import *
+#
+# tim = Turtle()
+# screen = Screen()
+#
+# def move_forwards():
+#     tim.forward(10)
+#
+# def move_right():
+#      tim.right(45)
+#
+# # def move_left():
+# #     tim.left(10)
+#
+# # def move_backwards():
+# #      tim.backward(10)
+#
+#
+# screen.listen()
+# screen.onkey(key="space", fun=move_forwards)
+# screen.onkey(key="Up", fun=move_right)
+# screen.exitonclick()
